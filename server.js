01d00d854f55603505cfcefe738cfa9df7de7b61
Jacob <jacobmgevans@gmail.com>
2018-05-01 16:49:35 -0700

    added dotenv and superagent

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
+require('dotenv').config();
+
 const superagent = require('superagent');
 const Koa = require('koa');
 const app = new Koa();
-const PORT = process.env.PORT || 8080;
-require('dotenv').config();
+const PORT = process.env.PORT;
 
 app.use(async (ctx, next) => {
   const start = Date.now();
   await next();
   const ms = Date.now() - start;
   ctx.set('X-Response-Time', `${ms}ms`);
+  console.log(ms);
+  ctx.header;
 });
 
-console.log();
-
-app.listen(PORT, `listening on PORT ${PORT}`);
+app.listen(PORT);
