a28d656b905eccf0af789cb05e3a80bc7bb83ff2
Jacob <jacobmgevans@gmail.com>
2018-05-24 10:43:10 -0700

    replaced koa with express

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 require('dotenv').config();
 
 const superagent = require('superagent');
-const Koa = require('koa');
-const app = new Koa();
+const Express = require('express');
+const app = new Express();
 const PORT = process.env.PORT;
 const AMAZON_API_SERVER = process.env.AMAZON_API_URL;
 
 //If superagent doesnt work with Koa refactor to Express. Figure out how the context Koa might work with superagent
-app.use(async (ctx, next) => {
-  console.log('REQUEST HERE', ctx.request, 'RESPONSE HERE', ctx.response);
-});
+app.use();
 
 // TWo GETs that will handle the API for Amazon for prices of (START with the LIGHTBULBS)
 // The second API will get specifications for those types of items if the specs for those things are not already found in the
